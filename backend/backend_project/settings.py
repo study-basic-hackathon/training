913121cbe08e26cbe20d13781683a79197f5c6c8
km-1 <kozanya.313131@gmail.com>
2025-05-18 22:00:16 +0900

    add frontend dir, edit settings.py, urls.py

--- backend/backend_project/settings.py
+++ backend/backend_project/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -54,7 +55,7 @@ ROOT_URLCONF = 'backend_project.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, '../frontend')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -64,6 +65,10 @@ TEMPLATES = [
             ],
         },
     },
+]
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, '../frontend'), # frontend/static を指定
 ]
 
 WSGI_APPLICATION = 'backend_project.wsgi.application'
