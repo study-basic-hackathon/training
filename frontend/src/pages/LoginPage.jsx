8d048786e94755679c2f55c096fc6e3db0166f2c
km-1 <kozanya.313131@gmail.com>
2025-05-29 17:07:20 +0900

    React Router, Header, LoginPage, DashboardPage

---
+++ frontend/src/pages/LoginPage.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const LoginPage = () => {
+    return (
+        <div>
+            <h1>トップページ / ログイン</h1>
+            <p>ここがトップページです。</p>
+            <button>ログイン</button>
+        </div>
+    );
+};
+
+export default LoginPage;
